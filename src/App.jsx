83bc371ea18fe738034985f204c95deadee2601a
Geovanny Mena <geovannymenavc@gmail.com>
2022-09-20 21:25:39 -0500

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import ProtectedRoutes from './components/routes/ProtectedRoutes'
 import { useDispatch } from 'react-redux'
 import { getAllProducts } from './store/slices/products.slice'
 import { useEffect } from 'react'
+import './footer.css'
 
 function App() {
 
@@ -30,6 +31,18 @@ function App() {
           <Route path='/cart' element={<Cart />} />
         </Route>
       </Routes>
+      <footer>
+        <div class="footer1">
+          <div class="footer2">
+            <h3 >Contact Us:</h3>
+          </div>
+          <ol class="footer3">
+            <a href="mailto:geomenacontact@gmail.com" target="_blank"><i class="fa-solid fa-envelope"></i></a>
+            <a href="https://www.linkedin.com/in/geovanni-mena-651b05240/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+            <a href="https://github.com/Geovamena" target="_blank"><i class="fa-brands fa-github"></i></a>
+          </ol>
+        </div>
+      </footer>
     </div>
   )
 }
